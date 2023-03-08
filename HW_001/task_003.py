2b4b172dcdf277ccb3c2e293966d025df24528e8
Nikolai <djaknvn@gmail.com>
2023-03-09 00:35:44 +0300

    сделал черех срез задачу 3 из 1 дз

--- HW_001/task_003.py
+++ HW_001/task_003.py
@@ -7,24 +7,12 @@
 # 385916 -> yes
 # 123456 -> no
 
-t = input('Введите номер билета: ')
-l = int(t[0]) + int(t[1]) + int(t[2])
-r = int(t[3]) + int(t[4]) + int(t[5])
-if l == r:
-    print('Yes')
+x = list(map(int, list(input())))
+
+left = sum(x[0:-3])
+right = sum(x[-3:])
+
+if left == right:
+    print("Счастливый")
 else:
-    print('NO')
-# или
-s = input('Введите 6-значный номер билета: ')
-if len(s) != 6:
-    print(f'Число {s} не 6-ти значное')
-else:
-    res1 = 0
-    res2 = 0
-    for i in range(len(s)//2):
-        res1 += int(s[i])
-        res2 += int(s[len(s)//2 + i])
-    if res1 == res2:
-        print(f'{s} счастливый номер')
-    else:
-        print(f'{s} не счастливый номер')
+    print("Несчастливый")
